af0a045b52e506f7063ec68cb6f687977ea642ca
stuart <stuart@stuarts-MBP-2.lan>
2026-03-29 19:15:55 -0400

    chore: remove temp OpenAPI regen example

--- engine/flapjack-http/examples/regen_openapi.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let path = flapjack_http::openapi_export::default_docs2_output_path();
-    flapjack_http::openapi_export::write_openapi_json(&path).unwrap();
-    println!("Regenerated {}", path.display());
-}
